6d2d00879fe7fde63bb3116983df05d37ad6d454
Anastasiya Rabetskaya <nastia1362@gmail.com>
2022-11-08 12:50:45 +0100

    useParams

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, NavLink, Route, Routes, useParams} from "react-router-dom";
+
+const Profile = () => {
+    const params = useParams();
+    const some = params;
+    console.log(some);
+    return (
+        <div>
+            profile
+        </div>
+    )
+}
 
 function App() {
     return (
-        <div className="App">
+        <BrowserRouter>
+            <div>
+                {/* links*/}
+                <NavLink to={"/"}>main</NavLink>
+                <NavLink to={"/login"}>login</NavLink>
+                <NavLink to={"/profile"}>profile</NavLink>
+                <NavLink to={"/profile/1"}>profile-1</NavLink>
 
-        </div>
+                <Routes>
+                    <Route path={'/'} element={<div>main</div>}/>
+                    <Route path={'/*'} element={<div>404</div>}/>
+                    <Route path={'/login'} element={<div>login</div>}/>
+                    {/*<Route path={'/profile/:id'} element={<Profile/>}/>*/}
+                    <Route path={'/profile/*'} element={<Profile/>}/>
+                </Routes>
+            </div>
+        </BrowserRouter>
+
     );
 }
 
